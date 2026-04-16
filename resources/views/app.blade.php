9df9249eb4bdf8f9edcd0d48d8ffa58d2632003d
Nik Fedorov <neudor@gmail.com>
2026-04-17 00:06:14 +0200

    feat: add antialiasing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,7 +31,7 @@
     <script>localStorage.setItem('api_token', '{{ $devToken }}');</script>
     @endif
 </head>
-<body>
+<body class="antialiased">
     <div id="app">
         <div id="splash" style="min-height:100vh;display:flex;align-items:center;justify-content:center;padding-bottom:20vh;background:#fff">
             <svg id="splash-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="none" width="64" height="64"><rect id="splash-bg" width="512" height="512" rx="96" fill="#f5f5f5"/><rect x="80" y="272" width="160" height="160" rx="24" fill="#14532d"><animate attributeName="opacity" values="0.4;1;0.4" dur="1.6s" begin="0s" repeatCount="indefinite"/></rect><rect x="272" y="272" width="160" height="160" rx="24" fill="#15803d"><animate attributeName="opacity" values="0.4;1;0.4" dur="1.6s" begin="0.2s" repeatCount="indefinite"/></rect><rect x="80" y="80" width="160" height="160" rx="24" fill="#16a34a"><animate attributeName="opacity" values="0.4;1;0.4" dur="1.6s" begin="0.4s" repeatCount="indefinite"/></rect><rect x="272" y="80" width="160" height="160" rx="24" fill="#22c55e"><animate attributeName="opacity" values="0.4;1;0.4" dur="1.6s" begin="0.6s" repeatCount="indefinite"/></rect></svg>
