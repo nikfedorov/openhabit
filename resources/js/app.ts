f93bc7791ff44fb3fbffee87386996c247f52fef
Nik Fedorov <neudor@gmail.com>
2026-05-02 00:36:27 +0200

    feat: integrate sentry to frontend

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,6 +1,20 @@
+import * as Sentry from '@sentry/vue';
 import { createApp } from 'vue';
 import '../css/app.css';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
+
+if (sentryDsn) {
+    Sentry.init({
+        app,
+        dsn: sentryDsn,
+        integrations: [Sentry.browserTracingIntegration({ router })],
+        tracesSampleRate: 1.0,
+    });
+}
+
+app.use(router).mount('#app');
