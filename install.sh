510f3f3fdf6c82be5196480d8c252cc29861e8b6
Nik Fedorov <neudor@gmail.com>
2026-05-17 17:38:17 +0200

    feat: update install script

--- install.sh
+++ install.sh
@@ -23,11 +23,11 @@ docker run --rm \
 echo -e "\n${bold}> sail build${normal}"
 vendor/bin/sail build
 
-echo -e "\n${bold}> sail artisan octane:install --server=frankenphp${normal}"
-vendor/bin/sail artisan octane:install --server=frankenphp
-
 echo -e "\n${bold}> sail up -d${normal}"
 vendor/bin/sail up -d
+
+echo -e "\n${bold}> sail artisan octane:install --server=frankenphp${normal}"
+vendor/bin/sail artisan octane:install --server=frankenphp
 
 echo -e "\n${bold}> sail artisan storage:link${normal}"
 vendor/bin/sail artisan storage:link
